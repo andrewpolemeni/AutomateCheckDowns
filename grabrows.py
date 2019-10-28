b351722d0670eec3ec9c3f03dd1a8042aace5e50
andrewpolemeni <46501718+andrewpolemeni@users.noreply.github.com>
2019-10-28 14:54:29 -0400

    Update grabrows.py

--- grabrows.py
+++ grabrows.py
@@ -6,18 +6,12 @@
 #==========================================================================================================================
 
 # Import statements
+import sys
 import pandas as pd
 import openpyxl
 from openpyxl.styles import PatternFill
 from openpyxl.styles.colors import YELLOW
-
-
-   
-
-#from pandas import ExcelWriter
-#from pandas import ExcelFile
-
- 
+import main
 
 #==========================================================================================================================
 
@@ -29,15 +23,13 @@ def GetStudentData(query_filename):
     # Answers a DataFrame
     # query = file name of excel spreadsheet containing query info
     # sid is student id
-
-   
-
     df1 = pd.read_excel(query_filename, sheet_name='sheet1', usecols="A, B, C, D, E, F, G, M") #df = dataframe variable and import file here
     df1.dropna(inplace=True) #drop cells with NaN
-    df1.drop(df2.loc[df1['Grade']=='F'].index, inplace=True) # Delete grades that are equal to F
-    df1.drop(df2.loc[df1['Grade']=='FN'].index, inplace=True) # Delete grades that are equal to FN
-    df1.drop(df2.loc[df1['Grade']=='W'].index, inplace=True) # Delete grades that are equal to W
-
+    df1.drop(df1.loc[df1['Grade']=='F'].index, inplace=True) # Delete grades that are equal to F
+    df1.drop(df1.loc[df1['Grade']=='FN'].index, inplace=True) # Delete grades that are equal to FN
+    df1.drop(df1.loc[df1['Grade']=='W'].index, inplace=True) # Delete grades that are equal to W
+    df1.drop(df1.loc[df1['Grade']=='D'].index, inplace=True) # Delete grades that are equal to D
+    df1.drop(df1.loc[df1['Grade']=='I'].index, inplace=True) # Delete grades that are equal to I
     return df1
 
  
@@ -52,17 +44,16 @@ def FilterForStudent(df1, sid):
 
     return df2   
 
-def CreateStudentFile(df):
-
 #==========================================================================================================================
 # File to be pasted into - this is a blank workbook
+def CreateStudentFile(df):
 
     file = "BSET"
     if (df['Acad Plan'].iloc[0] == 633400):
 
         file = "BSIT"
 
-    if (df['Acad Plan'].iloc[0] == 633300):
+    if (df['Acad Plan'].iloc[0] == 633300): # Find the dataframe by string and equal to program number
 
         file = "BSIT"
 
@@ -98,13 +89,12 @@ ids = df1['ID'].unique().tolist()
 
 
 # This will generate a spreadsheet for everyone
+def GenerateAllSpreadsheets():
+    for sid in ids:
+        df2 = FilterForStudent(df1, sid)  
+        file = CreateStudentFile(df2)
+        print("Student Information save to " + file)
 
-for sid in ids:
-    df2 = FilterForStudent(df1, sid)  
     file = CreateStudentFile(df2)
-    print("Student Information save to " + file)
- 
-
-file = CreateStudentFile(df2)
-print(df2['Acad Plan'].iloc[0])
-file = CreateStudentFile(df2)
+    print(df2['Acad Plan'].iloc[0])
+    file = CreateStudentFile(df2)
