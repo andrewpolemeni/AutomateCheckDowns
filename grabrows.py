3a011e8177fb42fe5aa2db38bd728e512cf3c93f
andrewpolemeni <46501718+andrewpolemeni@users.noreply.github.com>
2019-10-31 14:47:13 -0400

    Update grabrows.py
    program creates paths now and blacks out semesters.

--- grabrows.py
+++ grabrows.py
@@ -1,19 +1,45 @@
-#==========================================================================================================================
+#==================================================================================================================================================================================
 # Authors: Dr. Eaglin, Andrew Polemeni
 # Organization: Daytona State College
 # Date Written: October 24, 2019
 # Program Purpose: To automate course check downs for student progress.
-#==========================================================================================================================
-
+#==================================================================================================================================================================================
 # Import statements
-import sys
+import sys, os.path
 import pandas as pd
 import openpyxl
 from openpyxl.styles import PatternFill
-from openpyxl.styles.colors import YELLOW
+from openpyxl.styles.colors import YELLOW, BLACK
 import main
+#==================================================================================================================================================================================
+# NEED HELP GETTING VARIABLE FROM PYQT5
 
-#==========================================================================================================================
+#==================================================================================================================================================================================
+# Create Path Variables and Check if they exist
+
+mainPath = 'ATC_STUDENT_CHECKDOWNS/' # Create path for BSIT
+
+bsitPath = 'ATC_STUDENT_CHECKDOWNS/BSIT/'
+if not os.path.exists('ATC_STUDENT_CHECKDOWNS/BSIT/'):
+    os.makedirs(bsitPath)
+else:
+    print('This directory already exist' + ' ' + bsitPath)
+
+
+bsetPath = 'ATC_STUDENT_CHECKDOWNS/BSET/' # Create path for BSET
+if not os.path.exists('ATC_STUDENT_CHECKDOWNS/BSET/'):
+    os.makedirs(bsetPath)
+else:
+    print('This directory already exist'+ ' ' + bsetPath)
+
+
+bseetPath = 'ATC_STUDENT_CHECKDOWNS/BSEET/' # Create path for BSEET
+if not os.path.exists('ATC_STUDENT_CHECKDOWNS/BSEET/'):
+    os.makedirs(bseetPath)
+else:
+    print('This directory already exist' + ' ' + bseetPath)
+#==================================================================================================================================================================================
+
 
 #Grab the first excel file which will be the Query Report
 # Removes all columns and values that should not be used
@@ -23,16 +49,16 @@ def GetStudentData(query_filename):
     # Answers a DataFrame
     # query = file name of excel spreadsheet containing query info
     # sid is student id
-    df1 = pd.read_excel(query_filename, sheet_name='sheet1', usecols="A, B, C, D, E, F, G, M") #df = dataframe variable and import file here
+    
+    df1 = pd.read_excel(query_filename, sheet_name='sheet1', usecols="A, B, C, D, E, F, G, H, M, N") #df = dataframe variable and import file here
     df1.dropna(inplace=True) #drop cells with NaN
+    #df1.drop(df1.index[0]) # DROP the first row because of how the report generates from query
     df1.drop(df1.loc[df1['Grade']=='F'].index, inplace=True) # Delete grades that are equal to F
     df1.drop(df1.loc[df1['Grade']=='FN'].index, inplace=True) # Delete grades that are equal to FN
     df1.drop(df1.loc[df1['Grade']=='W'].index, inplace=True) # Delete grades that are equal to W
     df1.drop(df1.loc[df1['Grade']=='D'].index, inplace=True) # Delete grades that are equal to D
     df1.drop(df1.loc[df1['Grade']=='I'].index, inplace=True) # Delete grades that are equal to I
     return df1
-
- 
 
 def FilterForStudent(df1, sid):
     #Drop rows not needed or used
@@ -49,48 +75,59 @@ def FilterForStudent(df1, sid):
 def CreateStudentFile(df):
 
     file = "BSET"
-    if (df['Acad Plan'].iloc[0] == 633400):
+    if df['Acad Plan'].iloc[0] == 633400:
 
         file = "BSIT"
 
-    if (df['Acad Plan'].iloc[0] == 633300): # Find the dataframe by string and equal to program number
+    if df['Acad Plan'].iloc[0] == 633300: # Find the dataframe by string and equal to program number
 
         file = "BSIT"
 
     wb2 = openpyxl.load_workbook(file + ".xlsx")
     ws2 = wb2.active
+    
+    # Add first and last names
+    #ws2.cell(row=4, column=3).value = str(df.loc[0]["First Name"]) + " " + str(df.loc[0]["Last"])
+    #ws2.cell(row=5, column=3).value = str(df.loc[0]["ID"])
 
-#==========================================================================================================================
 # We now simply go through each column of the BSET Checkdown
 # and compare against courses taken
-    for i in range(1,60):
+    for i in range(1, 60):
+        # Define course variable
         course = str(ws2.cell(row=i, column=3).value)[0:7]
+
         for row in df.iterrows():
+            # Define taken and name variable
             taken = row[1]['Subject'] + str(row[1]['Catalog'])
             name = row[1]['First Name'] + "_" + str(row[1]['Last'])
 
-            if (course == taken):
-
+            if course == taken:
+                
                 ws2.cell(row=i, column=4).value = str(row[1]['Term'])
-                ws2.cell(row=i, column=4).fill = PatternFill(fgColor=YELLOW, fill_type = "solid")
+                ws2.cell(row=i, column=4).fill = PatternFill(fgColor=YELLOW, fill_type = "solid") #IF TAKEN CHECK OFF WITH YELLOW
+                
+                ws2.cell(row=i, column=5).fill = PatternFill(fgColor=BLACK, fill_type="solid") # Fill in semesters with black
+                ws2.cell(row=i, column=6).fill = PatternFill(fgColor=BLACK, fill_type="solid")
+                ws2.cell(row=i, column=7).fill = PatternFill(fgColor=BLACK, fill_type="solid")
+                ws2.cell(row=i, column=8).fill = PatternFill(fgColor=BLACK, fill_type="solid")
+                ws2.cell(row=i, column=9).fill = PatternFill(fgColor=BLACK, fill_type="solid")
+                ws2.cell(row=i, column=10).fill = PatternFill(fgColor=BLACK, fill_type="solid")
 
-
-#Once the sheet is filled out
-
+    #Once the sheet is filled out
     file_to_save = file + "_" + name + '.xlsx'
-    wb2.save(file_to_save)
+    wb2.save(mainPath + file_to_save)
     return file_to_save
+#==========================================================================================================================
 
 # Use of the functions
 # Get the ID of student for report
-
 df1 = GetStudentData("Query.xlsx") #CREATE A VARIABLE FOR THE QUERY SHEET
-ids = df1['ID'].unique().tolist()
+ids = df1['ID'].unique().tolist() # Creates a list of the ids
 
 
-# This will generate a spreadsheet for everyone
+# This will generate a spreadsheet for everyone by calling the functions.
 def GenerateAllSpreadsheets():
-    for sid in ids:
+    for sid in ids: # Iterate through all student ids in the ids list
         df2 = FilterForStudent(df1, sid)  
         file = CreateStudentFile(df2)
         print("Student Information save to " + file)
